5fded4c79903650cb96595a0d33a0d2617ffcabf
kimdongyoung <wwweric12@naver.com>
2025-12-22 10:11:27 +0900

    feat: Implement SSR for Popular Posts with React Query

--- apis/utils/queryKeys.ts
+++ apis/utils/queryKeys.ts
@@ -2,4 +2,5 @@ export const boardKeys = {
     all: ['board'] as const,
     posts: (page: number, size: number, categories: string[]) =>
         [...boardKeys.all, 'posts', { page, size, categories }] as const,
+    popular: () => [...boardKeys.all, 'popular'] as const,
 };
--- apis/utils/queryOptions.ts
+++ apis/utils/queryOptions.ts
@@ -2,4 +2,7 @@ export const queryOptions = {
     board: {
         staleTime: 60 * 1000, // 1 minute
     },
+    popular: {
+        staleTime: 5 * 60 * 1000, // 5 minutes
+    },
 } as const;
