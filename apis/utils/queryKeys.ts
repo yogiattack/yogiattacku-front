4c149e79499d15121b414c1a8a70a1fd8e7f052a
kimdongyoung <wwweric12@naver.com>
2025-12-24 13:04:38 +0900

    feat(user): add user api and types

--- apis/utils/queryKeys.ts
+++ apis/utils/queryKeys.ts
@@ -4,4 +4,10 @@ export const boardKeys = {
         [...boardKeys.all, 'posts', { page, size, categories }] as const,
     detail: (boardId: number) => [...boardKeys.all, 'detail', boardId] as const,
     popular: () => [...boardKeys.all, 'popular'] as const,
+    mypage: (page: number, size: number) => [...boardKeys.all, 'mypage', { page, size }] as const,
+};
+
+export const userKeys = {
+    all: ['user'] as const,
+    profile: () => [...userKeys.all, 'profile'] as const,
 };
